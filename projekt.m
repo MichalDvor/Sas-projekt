959a93389521e02fdb481d4d7eb1cded8bbca098
MichalDvor <mdvoracek3@gmail.com>
2025-11-01 16:59:54 +0100

    Uhlazení jedničky
    Dal jsem to do jednoho grafu a má to legendu, ale pořád nevím, jestli to je dobře, protože otravuje s imaginárníma částma v signal_opravený. Možná se to bude muset udělat poctivě a ne jen přes fft a ifft

--- projekt.m
+++ projekt.m
@@ -1,4 +1,3 @@
-
 %1
 clc 
 clearvars
@@ -10,40 +9,36 @@ signal_vzor_n = signal_vzor / max(max(signal_vzor),abs(min(signal_vzor))); % Nor
 t=linspace(0,length(signal_vzor_n)/frekvence_vzorkovani,length(signal_vzor_n)); % Casova konstanta vytvoreni vektoru
 
 
-signal_F =fft(signal_vzor);
+signal_F =fft(signal_vzor);                                     % Fourierova transformace
+
+signal_F_opraveny=signal_F;                                     % Vytvoreni opraveneho signalu
+frekvence_ruseni_h=16108;
+frekvence_ruseni_d=13900;
+signal_F_opraveny(frekvence_ruseni_d:frekvence_ruseni_h)=0;
+signal_F_opraveny(1170000:length(signal_F_opraveny))=0;         % Tohle odstrani to periodicke ruseni, nevim proc
+
+signal_opraveny=ifft(signal_F_opraveny);                        % Signal opraveny v casove oblasti
+signal_opraveny_n = signal_opraveny / max(max(signal_opraveny),abs(min(signal_opraveny))); % Normalizace
 
 figure(1);                                                      % Plocha pro graf
 subplot(2,1,1);                                                 % Rozdeleni na 2 radky, 1 sloupec
-plot(t,signal_vzor_n);                                          % Vykresleni vzoru signalu 
+plot(t,signal_vzor_n, t, signal_opraveny_n);                       % Vykresleni vzoru a opraveneho signalu v casove 
 title("Signal vzor normovany");
 xlabel("t [s]");
 ylabel("normovana amplituda");
+legend("Vzor", "Opraveno");
 
+frekvence=1:length(signal_F);
 
-subplot(2,1,2);                                                 % Druhy graf
-loglog(abs(signal_F));                                          % Graf obrazu ve fourierovi, ale obe logaritmicke
+subplot(2,1,2);                                                         % Druhy graf
+loglog(frekvence, abs(signal_F),frekvence, abs(signal_F_opraveny));     % Graf obrazu frekvencni, ale obe logaritmicke
+legend("Vzor", "Opraveno");
 
-frekvence=0:length(signal_F);
-frekvence_ruseni_h=16108;
-frekvence_ruseni_d=13900;
-signal_F_opraveny=signal_F;                                  %abs(signal_F).*(okno((frekvence-frekvence_ruseni)*(1/sirka_pasma_ruseni)));
-signal_F_opraveny(frekvence_ruseni_d:frekvence_ruseni_h)=0;
-signal_F_opraveny(1170000:length(signal_F_opraveny))=0;      %tohle funguje
+%frekvence=0:length(signal_F);
+%frekvence_ruseni_h=16108;
+%frekvence_ruseni_d=13900;
+%signal_F_opraveny=signal_F;                                  %abs(signal_F).*(okno((frekvence-frekvence_ruseni)*(1/sirka_pasma_ruseni)));
+%signal_F_opraveny(frekvence_ruseni_d:frekvence_ruseni_h)=0;
+%signal_F_opraveny(1170000:length(signal_F_opraveny))=0;      %tohle funguje
 
-
-signal_opraveny=ifft(signal_F_opraveny);
-t=linspace(0,length(signal_opraveny)/frekvence_vzorkovani,length(signal_opraveny)); % Casova konstanta vytvoreni vektoru
-
-figure(2);                                                      % Plocha pro graf
-subplot(2,1,1);                                                 % Rozdeleni na 2 radky, 1 sloupec
-plot(t,signal_opraveny);                                          % Vykresleni vzoru signalu 
-title("Signal opraven");
-xlabel("t [s]");
-ylabel("normovana amplituda");
-
-
-subplot(2,1,2);                                                 % Druhy graf
-loglog(abs(signal_F_opraveny)); 
-
-
-audiowrite("opraveno.wav",real(signal_opraveny),frekvence_vzorkovani);
+audiowrite("opraveno.wav",real(signal_opraveny),frekvence_vzorkovani); % Opraveny audio zaznam
