c407ba33abd2e7431e647325ea7054af2cda66ca
MichalDvor <mdvoracek3@gmail.com>
2025-11-05 13:38:53 +0100

    Update fourierVlastni.m
    Oddělání warningu

--- fourierVlastni.m
+++ fourierVlastni.m
@@ -1,6 +1,6 @@
 function signalF = fourierVlastni(signal, time, period)
 w0=2*pi/period;
-[a,velikost]=size(time);
+[~,velikost]=size(time);
 suma=zeros(size(time));
 for k=1:velikost
     suma(k)=sum(signal.*exp(-1j*w0*k*time(k)));
@@ -11,3 +11,4 @@ for m = 1:velikost
 end
 signalF=sigF;
 end
+
