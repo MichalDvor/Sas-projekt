6b3c0ab0673654e4be7ae30d851a229a0aa9acfa
MichalDvor <mdvoracek3@gmail.com>
2025-11-05 13:36:54 +0100

    Update fourierVlastni.m
    Opraveno, aby to alespoň něco dělalo

--- fourierVlastni.m
+++ fourierVlastni.m
@@ -1,13 +1,13 @@
 function signalF = fourierVlastni(signal, time, period)
 w0=2*pi/period;
-M=size(time);
+[a,velikost]=size(time);
 suma=zeros(size(time));
-for k=1:size(time)
+for k=1:velikost
     suma(k)=sum(signal.*exp(-1j*w0*k*time(k)));
 end
-signalF=zeros(M);
-for m = 1:M
- signalF(m)=1/period*suma(m);
+sigF=zeros(M);
+for m = 1:velikost
+ sigF(m)=1/period*suma(m);
 end
-
+signalF=sigF;
 end
